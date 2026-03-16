5d4efb54f0cff7c03a3c637f4175e48a386634d2
Florenzo Bauer <florenzobauer@gmail.com>
2026-03-15 19:23:27 -0600

    Initial Commit

---
+++ Timesheet/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Timesheet
+//
+//  Created by Florenzo Bauer on 3/15/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
